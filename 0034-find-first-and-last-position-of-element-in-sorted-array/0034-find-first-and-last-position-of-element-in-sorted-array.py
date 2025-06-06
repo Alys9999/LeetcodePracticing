04ef4fa85f7941ff3c3596329e71ce6229eb73db
Alys9999 <74774427+Alys9999@users.noreply.github.com>
2025-06-06 03:42:36 -0700

    Time: 0 ms (100%), Space: 19.1 MB (52.93%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,34 +1,21 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def findLeft(nums, target):
+        n = len(nums) - 1
+        def binary(isLeft):
             left = 0
-            right = len(nums) -1
+            right = n
+            res = -1
             while left <= right:
-                mid = left + (right- left)//2
-                if nums[mid] < target:
+                mid = left + (right - left)//2
+                if nums[mid] == target:
+                    res = mid
+                    if isLeft:
+                        right = mid -1
+                    else:
+                        left = mid + 1
+                elif nums[mid] < target:
                     left = mid + 1
                 elif nums[mid] > target:
                     right = mid -1
-                elif nums[mid] == target:
-                    right = mid - 1
-            if left < 0 or left > len(nums)-1 or nums[left] != target:
-                return -1
-            return left
-
-        def findRight(nums, target):
-            left = 0
-            right = len(nums)-1
-            while left <=right:
-                mid = left + (right-left)//2
-                if nums[mid] < target:
-                    left = mid + 1
-                elif nums[mid] > target:
-                    right = mid -1
-                elif nums[mid] == target:
-                    left = mid + 1
-            if left-1 < 0 or left - 1 > len(nums)-1 or nums[left-1] != target:
-                return -1
-            return left -1
-        leftb = findLeft(nums, target)
-        rightb = findRight(nums, target)
-        return [leftb, rightb]
+            return res
+        return [binary(True), binary(False)]
