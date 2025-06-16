850fc2af6f164a97fb32093bcbc30f6527cd75ee
Alys9999 <74774427+Alys9999@users.noreply.github.com>
2025-06-16 02:01:29 -0700

    Time: 3 ms (46.22%), Space: 17.8 MB (50%) - LeetHub

---
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        cur = 1
+        preorder = split(',', preorder)
+        print(preorder)
+        for node in preorder:
+            if node == '#':
+                cur -= 1
+                if cur < 0:
+                    return False
+            else: 
+                cur -= 1
+                if cur < 0:
+                    return False
+                cur += 2
+            
+        return cur == 0
